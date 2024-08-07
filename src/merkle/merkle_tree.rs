eb42499f47cbd3c3357fda5ba27415b7b7830bc0
Paolo Belforte <pbelforte2003@gmail.com>
2024-08-07 12:35:28 -0300

    Tree structure for Merkle

--- src/merkle/merkle_tree.rs
+++ src/merkle/merkle_tree.rs
@@ -1,11 +1,73 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
 
+#[derive(Clone)]
+struct MerkleNode {
+    hash_value: u64,
+    left: Option<Box<MerkleNode>>,
+    right: Option<Box<MerkleNode>>,
+}
 struct MerkleTree {
-    transactions_hash: Vec<u64>,
+    merkle_root: MerkleNode,
+}
+
+impl MerkleNode {
+    pub fn new(hash_value: u64) -> Self {
+        Self {
+            hash_value,
+            left: None,
+            right: None,
+        }
+    }
 }
 
 impl MerkleTree {
-    pub fn new<H: Hash>(transactions: Vec<H>) -> Self {
+    pub fn new<H: Hash>(transactions: Vec<H>) -> Result<Self, String> {
+        if transactions.is_empty() {
+            return Err("Empty transactions vector".into());
+        }
+
+        let transactions_hash = Self::get_hashes(transactions);
+
+        Ok(Self::create_tree_from_hashes(transactions_hash))
+    }
+
+    fn create_tree_from_hashes(transactions_hash: Vec<u64>) -> MerkleTree {
+        let mut leafs: Vec<Box<MerkleNode>> = Vec::new();
+        for hash in transactions_hash {
+            leafs.push(Box::new(MerkleNode::new(hash)));
+        }
+
+        while leafs.len() > 1 {
+            let mut parents = Vec::new();
+
+            for _ in (0..leafs.len()).step_by(2) {
+                let mut hasher = DefaultHasher::new();
+                let left = leafs.pop();
+                let right = leafs.pop();
+
+                if let Some(left) = &left {
+                    left.hash_value.hash(&mut hasher);
+                }
+
+                if let Some(right) = &right {
+                    right.hash_value.hash(&mut hasher);
+                }
+
+                let mut parent = MerkleNode::new(hasher.finish());
+                parent.left = left;
+                parent.right = right;
+                parents.push(Box::new(parent));
+            }
+
+            leafs = parents;
+        }
+
+        Self {
+            merkle_root: *leafs[0].clone(),
+        }
+    }
+
+    fn get_hashes<H: Hash>(transactions: Vec<H>) -> Vec<u64> {
         let mut transactions_hash = Vec::new();
         for transaction in transactions {
             let mut hasher = DefaultHasher::new();
@@ -13,8 +75,7 @@ impl MerkleTree {
             let hash = hasher.finish();
             transactions_hash.push(hash);
         }
-
-        Self { transactions_hash }
+        transactions_hash
     }
 
     pub fn contains<H: Hash>(&mut self, transaction: H) -> bool {
@@ -23,7 +84,7 @@ impl MerkleTree {
         transaction.hash(&mut hasher);
         let hash = hasher.finish();
 
-        self.transactions_hash.contains(&hash)
+        self.merkle_root.hash_value == hash
     }
 }
 
@@ -36,14 +97,14 @@ pub mod test {
     fn test_001_a_new_merkle_tree_contains_nothing() {
         let transactions: Vec<String> = Vec::new();
         let mut merkle_tree = MerkleTree::new(transactions);
-        let transaction_not_in_tree = String::from("Hi");
-        assert!(!merkle_tree.contains(transaction_not_in_tree));
+
+        assert!(merkle_tree.is_err());
     }
 
     #[test]
     fn test_002_a_merkle_tree_can_contains_one_transaction() {
         let transactions = vec![String::from("hi")];
-        let mut merkle_tree = MerkleTree::new(transactions.clone());
+        let mut merkle_tree = MerkleTree::new(transactions.clone()).unwrap();
         let transaction = transactions[0].clone();
         assert!(merkle_tree.contains(transaction));
     }
@@ -51,7 +112,7 @@ pub mod test {
     #[test]
     fn test_003_a_merkle_tree_can_contains_multiple_transactions() {
         let transactions = vec![String::from("hi"), String::from("bye")];
-        let mut merkle_tree = MerkleTree::new(transactions.clone());
+        let mut merkle_tree = MerkleTree::new(transactions.clone()).unwrap();
         let transaction = transactions[0].clone();
         let another_transaction = transactions[1].clone();
         assert!(merkle_tree.contains(transaction));
